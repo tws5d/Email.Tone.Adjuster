478b235f83020bcca134230c6f3cd77cad4ef65f
tws5d <tylersnead85@gmail.com>
2025-06-02 11:10:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with col1:
 
 with col_msg:
     st.markdown(
-        "<p style='font-size: 0.85rem; color: gray; margin-top: 35px;'>"
+        "<p style='font-size: 0.85rem; color: gray; margin-top: 55px;'>"
         "☕ Enjoying the app?<br>Help support it.</p>",
         unsafe_allow_html=True
     )
